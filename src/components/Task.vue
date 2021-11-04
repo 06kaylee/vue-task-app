05fd33e29f532014e05fefafcf2aa36198b13f1b
Kaylee <06.kaylee@gmail.com>
2021-11-03 21:06:36 -0600

    initial commit

---
+++ src/components/Task.vue
@@ -0,0 +1,36 @@
+<template>
+    <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>{{ task.text }}
+          <button @click="$emit('delete-task', task.id)">X</button>
+        </h3>
+        <p>{{ task.day }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Task",
+    props: {
+        task: Object
+    },
+    methods: {
+    }
+}
+</script>
+
+<style scoped>
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
