6b57ef721f26019dcc2cd192542660a9170926fc
jsouthwick01 <47395926+jsouthwick01@users.noreply.github.com>
2021-12-02 17:38:07 -0700

    No Defaults
    Got rid of the default values, since it conflicts with the new format for the input. However, we can just ignore this.

--- src/App.vue
+++ src/App.vue
@@ -35,28 +35,6 @@ export default {
     toggleReminder(id) {
       this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task);
     }
-  },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: "Doctor's Appointment",
-        day: "March 1st at 2:30pm",
-        reminder: true
-      },
-      {
-        id: 2,
-        text: "Meeting at School",
-        day: "March 3rd at 1:30pm",
-        reminder: true
-      },
-      {
-        id: 3,
-        text: "Food Shopping",
-        day: "March 3rd at 2:30pm",
-        reminder: false
-      }
-    ]
   }
 }
 </script>
